82feb02ca992c0b1e67c99804dd1955d4a19c41d
Roopesh Rokade <04012001roopesh@gmail.com>
2023-08-17 12:00:02 +0530

    Initial comment for Task tracker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskComponent } from './components/task/task.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TaskFormComponent,
+    TaskListComponent,
+    TaskComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
